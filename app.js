4cb1fb3d05715ee7120b6047cbf4f6afca05ed10
AduJoseph <iamjosephadu@gmail.com>
2018-10-12 07:33:25 -0700

    add pug template engine

--- app.js
+++ app.js
@@ -5,11 +5,15 @@ const nodemailer = require('nodemailer');
 
 const app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug')
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World </h1>')
+    // res.send('<h1>Hello World Saved</h1>')
+    res.render('index');
 });
 
 app.listen(3000);
